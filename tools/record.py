7a1753bd897085225230fcadab86b9fbc4d868af
lee-pc\admin <584897688@qq.com>
2019-04-10 18:54:38 +0800

    Update record.py

--- tools/record.py
+++ tools/record.py
@@ -1,88 +1,148 @@
-import cv2
-import numpy
-import time
+#-*-coding:utf-8-*-
+import sys
 import os
+__pyBoost_root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(__pyBoost_root_path)
+import pyBoost as pb
+
+import cv2
 import datetime
+import argparse
+
+
 
 RECORD_CONFIG = \
 {
+    # open param
     'OPEN':(0,1),\
-    'PUBLIC_CAPTURE_SET':{cv2.CAP_PROP_FRAME_WIDTH:1280,\
-                        cv2.CAP_PROP_FRAME_HEIGHT:720,\
-                        cv2.CAP_PROP_FPS:25,\
-                        cv2.CAP_PROP_FOURCC:cv2.VideoWriter_fourcc(*'MJPG')},\
-    'PUBLIC_WRITER_SET':{'filename':'v{0}_{1}_{2}.avi',\
-                        'fourcc':cv2.VideoWriter_fourcc(*'MJPG'),\
+    'PUBLIC_CAPTURE_SET':{'cv2.CAP_PROP_FRAME_WIDTH':'1280',\
+                        'cv2.CAP_PROP_FRAME_HEIGHT':'720',\
+                        'cv2.CAP_PROP_FPS':'25',\
+                        'cv2.CAP_PROP_FOURCC':'cv2.VideoWriter_fourcc(*\'MJPG\')'},\
+    'PRIVATE_CAPTURE_SET':(),\
+
+    # save image param
+    'SAVE_FILE_NAME':'v{0}_{1}.jpg',\
+
+    # save video param
+    'PUBLIC_WRITER_SET':{'filename':'v{0}_{1}.avi',\
+                        'fourcc':'cv2.VideoWriter_fourcc(*\'MJPG\')',\
                         'fps':None,\
                         'frameSize':None},\
-    'PRIVATE_CAPTURE_SET':({},{}),\
-    'PRIVATE_WRITER_SET':({},{}),\
-    'SAVE_FILE_EXT':'.jpg'
-
-    
+    'PRIVATE_WRITER_SET':()
 }
+
+def decode_config(config=RECORD_CONFIG):
+    result = config.copy()
+    result['PUBLIC_CAPTURE_SET'] = {eval(k):eval(config['PUBLIC_CAPTURE_SET'][k]) for k in config['PUBLIC_CAPTURE_SET'].keys()}
+    result['PUBLIC_WRITER_SET'] = config['PUBLIC_WRITER_SET'].copy()
+    if result['PUBLIC_WRITER_SET'].get('fourcc') is not None:
+        result['PUBLIC_WRITER_SET']['fourcc'] = eval(result['PUBLIC_WRITER_SET']['fourcc'])
+    else:
+        result['PUBLIC_WRITER_SET']['fourcc'] = None
+
+    result['PRIVATE_CAPTURE_SET']=[{} for x in config['OPEN']]
+    if config.get('PRIVATE_CAPTURE_SET') is not None:
+        for i, one_open_set in enumerate(config['PRIVATE_CAPTURE_SET']):
+            if i>= len(sult['PRIVATE_CAPTURE_SET']):
+                break
+            result['PRIVATE_CAPTURE_SET'][i]={eval(k):eval(one_open_set[k]) for k in one_open_set.keys()}
+
+    result['PRIVATE_WRITER_SET']=[{} for x in config['OPEN']]
+    if config.get('PRIVATE_WRITER_SET') is not None:
+        for i, one_open_set in enumerate(config['PRIVATE_WRITER_SET']):
+            if i>= len(sult['PRIVATE_WRITER_SET']):
+                break
+            result['PRIVATE_WRITER_SET'][i]={eval(k):eval(one_open_set[k]) for k in one_open_set.keys()}
+    return result
+
+def get_date_time_string():
+    date_time_str = str(datetime.datetime.now())[:-7]
+    date_str,time_str= date_time_str.split()
+    h,m,s = time_str.split(':')
+    return date_str+'_'+h+'-'+m+'-'+s # such as 2019-11-11_11-11-11
+    
+    
 
 
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
-    #TODO
-    parser.add_argument('--dir', type = str,required=True, help = 'Where is the images?')
-    parser.add_argument('--wh', type = str,required=True, help = '[width,height] such as \'[720,480]\' for resized images')
-    parser.add_argument('--rtype', type = int, default = 1, help = '[0 = stretch.] [1 = round up.] [2 = round up and crop] [3 = round down] [4 = round down and fill black] [5 = round down and fill self], \"1\"')
-    parser.add_argument('--inter', default = 1,  type = int, help = '[0 = INTER_NEAREST] [1 = INTER_LINEAR] [2 = INTER_CUBIC] [3 = INTER_AREA] [4 = INTER_LANCZOS4], \"1\"')
-    parser.add_argument('--save', type = str,  help = 'Where to save. \"$dir$\"')
-    parser.add_argument('--wait', type = int, default=1, help = 'Where to save. \"$dir$\"')
-    parser.add_argument('-q','--quiet',nargs='?',default='NOT_MENTIONED',help='Sure to cover the source file? \"OFF\"')
-    parser.add_argument('-r','--recursion',nargs='?',default='NOT_MENTIONED',help='Whether scan file with recursion? \"OFF\"')
-
-   
-
+    parser.add_argument('--save', type = str,  help = 'Where to save. \"$pwd$\"')
+    parser.add_argument('-i','--image',nargs='?',default='NOT_MENTIONED',help='To save images? \"OFF\"')
+    parser.add_argument('-v','--video',nargs='?',default='NOT_MENTIONED',help='To save videos? \"OFF\"')
+    parser.add_argument('-t','--thread',nargs='?',default='NOT_MENTIONED',help='open camera in thread? \"OFF\"')
+    parser.add_argument('-q','--quiet',nargs='?',default='NOT_MENTIONED',help='Accept all the configuration? \"OFF\"')
     args = parser.parse_args()
 
+    # save dir
+    date_time = get_date_time_string()
+    if args.save is None:
+        save_dir = date_time
+    else:
+        save_dir = os.path.join(args.save, date_time)
+    pb.makedirs(save_dir)
+    if os.path.isdir(save_dir)==False:
+        sys.exit('Invalid save dir: {0}'.format(save_dir))
+
+    # config
+    for k in RECORD_CONFIG.keys():
+        print('{0}: {1}'.format(k,RECORD_CONFIG[k]))
+    if args.quiet =='NOT_MENTIONED':
+        print('')
+        print('Accept all the configurations above? [Y/N]?')
+        str_in = input()
+        if(str_in !='Y' and str_in !='y'):
+            sys.exit('user quit')
+    config = decode_config(RECORD_CONFIG)
+    for k in config.keys():
+        print('{0}: {1}'.format(k,config[k]))
+
+    # cap
+    #if args.thread =='NOT_MENTIONED':
+    #    cap_list = [pb.video.VideoCaptureRelink(x) for x in config['OPEN']]
+    #else:
+    #    cap_list = [pb.video.VideoCaptureThreadRelink(x) for x in config['OPEN']]
+    
+    
+    #if args.thread =='NOT_MENTIONED'
 
 
-to_open_list = [1,2]
-
-to_open_list = ['/home/lee/PycharmProject/noSensePay_bg/log/2019-03-14_15-57-2/rendered_2019-03-14_15-57-2_v0.avi',\
-'/home/lee/PycharmProject/noSensePay_bg/log/2019-03-14_15-57-2/rendered_2019-03-14_15-57-2_v1.avi']
-
-cap_list = []
-for x in to_open_list:
-    cap = cv2.VideoCapture(x)
-    if cap.isOpened():
-        #TODO: set
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
-        cap.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc('M','J','P','G'))
-        cap_list.append(cap)
+#for x in to_open_list:
+#    cap = cv2.VideoCapture(x)
+#    if cap.isOpened():
+#        #TODO: set
+#        cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
+#        cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
+#        cap.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc('M','J','P','G'))
+#        cap_list.append(cap)
 
 
-tt = datetime.datetime.now()
-datetime = str(tt)[0:10] + '_' + str(tt.hour) + '-' + str(tt.minute)+ '-' + str(tt.second)
-save_path = './'+datetime
-print(save_path)
-if os.path.isdir(save_path)==False:
-    os.makedirs(save_path)
+#tt = datetime.datetime.now()
+#datetime = str(tt)[0:10] + '_' + str(tt.hour) + '-' + str(tt.minute)+ '-' + str(tt.second)
+#save_path = './'+datetime
+#print(save_path)
+#if os.path.isdir(save_path)==False:
+#    os.makedirs(save_path)
 
-ret = True
-key = 0
-img_list = [0]*len(cap_list)
-index = 10000
-while key!=ord('q') and key!=ord('Q'):
-    #read
-    img_list = [cap.read() for cap in cap_list]
-    #show
-    for i,x in enumerate(img_list):
-        if x[0]==True:
-            cv2.imshow('v'+str(i),x[1])
-    #save
-    key = cv2.waitKey(44)
-    if key==ord('s') or key==ord('S'):
-        for i,x in enumerate(img_list):
-            if x[0]==True:
-                cv2.imwrite('{0}/{1}_v{2}_{3}.jpg'.format(save_path,datetime,i,index),x[1]) 
-        index += 1
+#ret = True
+#key = 0
+#img_list = [0]*len(cap_list)
+#index = 10000
+#while key!=ord('q') and key!=ord('Q'):
+#    #read
+#    img_list = [cap.read() for cap in cap_list]
+#    #show
+#    for i,x in enumerate(img_list):
+#        if x[0]==True:
+#            cv2.imshow('v'+str(i),x[1])
+#    #save
+#    key = cv2.waitKey(44)
+#    if key==ord('s') or key==ord('S'):
+#        for i,x in enumerate(img_list):
+#            if x[0]==True:
+#                cv2.imwrite('{0}/{1}_v{2}_{3}.jpg'.format(save_path,datetime,i,index),x[1]) 
+#        index += 1
 
 
 

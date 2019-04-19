b9966a6da2e84e2df1a2253341a1c25d2844b067
lee-pc\admin <584897688@qq.com>
2019-04-19 21:08:39 +0800

    Sort in video(not finish)

--- pyBoost/video.py
+++ pyBoost/video.py
@@ -1,9 +1,187 @@
 #-*-coding:utf-8-*-
+
+"""
+    SORT: A Simple, Online and Realtime Tracker
+    Copyright (C) 2016 Alex Bewley alex@dynamicdetection.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
 import cv2
 import numpy as np
 import threading
 import time
 import os
+
+import voc as pbvoc
+
+#from scipy.optimize import linear_sum_assignment
+from sklearn.utils.linear_assignment_ import linear_assignment
+
+class KalmanBoxTracker:
+    """
+    This class represents the internel state of individual tracked objects observed as bbox.
+    """
+    count = 0
+    maxsize = sys.maxsize-1000
+    id_lock = threading.Lock()
+    def __init__(self,bbox,id=None):
+        """
+        Initialises a tracker using initial bounding box.
+        """
+        #define constant velocity model
+        self.kf = KalmanFilter(dim_x=7, dim_z=4)
+        self.kf.F = np.array([[1,0,0,0,1,0,0],[0,1,0,0,0,1,0],[0,0,1,0,0,0,1],[0,0,0,1,0,0,0],  [0,0,0,0,1,0,0],[0,0,0,0,0,1,0],[0,0,0,0,0,0,1]])
+        self.kf.H = np.array([[1,0,0,0,0,0,0],[0,1,0,0,0,0,0],[0,0,1,0,0,0,0],[0,0,0,1,0,0,0]])
+        
+        self.kf.R[2:,2:] *= 10.
+        self.kf.P[4:,4:] *= 1000. #give high uncertainty to the unobservable initial velocities
+        self.kf.P *= 10.
+        self.kf.Q[-1,-1] *= 0.01
+        self.kf.Q[4:,4:] *= 0.01
+
+        self._pre_z = self._convert_bbox_to_z(bbox)
+        self.kf.x[:4] = self._pre_z
+
+        self.time_since_update = 0
+        if id is None:
+            KalmanBoxTracker.id_lock.acquire()
+            self.id = KalmanBoxTracker.count
+            KalmanBoxTracker.count += 1
+            if KalmanBoxTracker.count>KalmanBoxTracker.maxsize:
+                KalmanBoxTracker.count = 0
+            KalmanBoxTracker.id_lock.release()
+        else:
+            self.id = id
+        self.age = 0
+        self.predict_time_since_update = 0
+        self.update_time_since_predict = 0
+
+    def _convert_bbox_to_z(self,bbox):
+        """
+        Takes a bounding box in the form [x1,y1,x2,y2] and returns z in the form
+          [x,y,s,r] where x,y is the centre of the box and s is the scale/area and r is
+          the aspect ratio
+        """
+        w = bbox[2] - bbox[0]
+        h = bbox[3] - bbox[1]
+        x = bbox[0] + w / 2.
+        y = bbox[1] + h / 2.
+        s = w * h    #scale is just area
+        r = w / h
+        return np.array([x,y,s,r]).reshape((4,1))
+
+    def _convert_x_to_bbox(self,x):
+        """
+        Takes a bounding box in the centre form [x,y,s,r] and returns it in the form
+          [x1,y1,x2,y2] where x1,y1 is the top left and x2,y2 is the bottom right
+        """
+        w = np.sqrt(x[2] * x[3])
+        h = x[2] / w
+        return np.array([x[0] - w / 2.,x[1] - h / 2.,x[0] + w / 2.,x[1] + h / 2.]).reshape((1,4))
+       
+    def update(self,bbox):
+        """
+        Updates the state vector with observed bbox.
+        """
+        self.predict_time_since_update = 0
+        self.update_time_since_predict += 1        
+        self.pre_z = self._convert_bbox_to_z(bbox)
+        self.kf.update(self.pre_z)
+        self.age += 1
+        return
+    
+    def predict(self):
+        """
+        Advances the state vector and returns the predicted bounding box estimate.
+        """
+        if (self.kf.x[6] + self.kf.x[2]) <= 0:
+            self.kf.x[6] *= 0.0
+        self.kf.predict()
+        self.update_time_since_predict = 0
+        self.predict_time_since_update += 1
+        return self._convert_x_to_bbox(self.kf.x)
+
+    def stay(self):
+        self.kf.update(self._pre_z)
+        return 
+
+    def get_state(self):
+        """
+        Returns the current bounding box estimate.
+        """
+        return self._convert_x_to_bbox(self.kf.x)
+
+
+def _associate_detections_to_predictions(detections,predictions,iou_threshold):
+    """
+    Assigns detections to tracked object (both represented as bounding boxes)
+    Returns a list of matches_pairs
+    """
+
+    if len(predictions) == 0 or len(detections) == 0:
+        return np.empty((0,2),dtype=np.int32)
+
+    iou_matrix = pbvoc.exIouMatrix(detections,predictions)
+
+    matched_pairs = sklearn.utils.linear_assignment_.linear_assignment(-iou_matrix)
+    
+    #del low IOU
+    matches = []
+    for m in matched_pairs:
+        if iou_matrix[m[0],m[1]] < iou_threshold:
+            matches.append(False)
+        else:
+            matches.append(True)
+    return matched_pairs[matches].reshape(-1,2)
+
+# max_objs
+class Sort():
+    def __init__(self,max_loss_times=5,min_age=3,iou_thresh=0.3,max_objs=0):
+        """
+        Sets key parameters for SORT
+        """
+        self._max_loss_times = max_loss_times
+        self._min_age = min_age
+        self._iou_thresh = iou_thresh
+        self._max_objs = max_objs
+        self._trackers = [KalmanBoxTracker()]*0
+
+    def update(self,dets):
+        """
+        Params:
+          dets - a numpy array of detections in the format [[x1,y1,x2,y2,score],[x1,y1,x2,y2,score],...]
+        Requires: this method must be called once for each frame even with empty detections.
+        Returns (dets with id in last column, trk prediction with id in last column)， id = -1 means unmatched
+        """
+        #get predicted locations from existing _trackers.
+        preds = []
+        for t in range(len(self._trackers)-1,-1,-1):
+            one_trk = self._trackers[t]
+            pos = one_trk.predict()[0]
+            if(np.any(np.isnan(pos))):
+                self._trackers.pop(t)
+            else:
+                preds.append(np.array([pos[0], pos[1], pos[2], pos[3], one_trk.id]))
+        # TODO
+        #if len(dets)==0:
+        #    out_dets = np.empty((0,5),dtyep=np.int)
+        #else:
+            
+        
+
 
 class _VideoCaptureBase():
     def __init__(self, filename=None):

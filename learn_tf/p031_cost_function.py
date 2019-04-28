3304c9d6a6d421cbf7d151187f2a016e822a9839
DESKTOP-A6DCAT3\58489 <584897688@qq.com>
2019-04-29 01:21:16 +0800

    softmax cross entropy

--- learn_tf/p031_cost_function.py
+++ learn_tf/p031_cost_function.py
@@ -14,7 +14,6 @@ PREDICTED_VALUE = None
 cost = tf.reduce_mean(OPERATION_NAME(labels=ACTUAL_VALUE,logits=PREDICTED_VALUE))
 updates_sgd = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(cost)
 
-# some cost functions
 
 '''
 1. sigmoid cross entropy 
@@ -27,8 +26,28 @@ c] H(x,z) = z*log(S(x)**-1) + (1-z)*log((1-S(x))**-1)
           = log(1+exp(-x)) + x(1-z)    <==>    log(1+exp(x)) - x*z
 d] H(x,z) = max(x,0) - x*z + log(1+exp(-abs(x)))  # avoid overflows
 '''
+# _sentinel: placehold parameter, do not set it
+# labels: z = labels, labels.shape == logits.shape， labels.dtype==tf.float32 or labels.dtype==tf.float64 
+# logits: x = logits, logits.shape == labels.shape， logits.dtype==tf.float32 or logits.dtype==tf.float64 
+tf.nn.sigmoid_cross_entropy_with_logits(_sentinel=None, labels=None,logits=None,name=None)
 
 
+'''
+1. softmax cross entropy 
+a] S(x) = softmax(x) = exp(xi)/reduce_sum(exp(x)), S'(x) = S(x)*(1-S(x))
+b] x = logits, z = labels
+c] H(x,z)  = z*log(S(x)**-1) + (1-z)*log((1-S(x))**-1)
+d] dH(x,z)   
+     dx    = -z*S'(x)/S(x) + (z-1)*(1-S'(x))/(1-S(x))
+           = -z*(1-S(x)) + (1-z)*S(x)
+           = S(x) - z
+'''
+
+# _sentinel: placehold parameter, do not set it
+# labels: z = labels, labels.shape == logits.shape， labels.dtype==tf.float32 or labels.dtype==tf.float64 
+# logits: x = logits, logits.shape == labels.shape， logits.dtype==tf.float32 or logits.dtype==tf.float64 
+# dim: which dimension does softmax do?
+tf.nn.softmax_cross_entropy_with_logits_v2(_sentinel=None, labels=None,logits=None,dim=-1,name=None)
 
 
 

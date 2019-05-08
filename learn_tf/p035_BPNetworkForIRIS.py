6057da1cf39a926cb1f1228128112201077c3a8a
lee-pc\admin <584897688@qq.com>
2019-05-08 21:06:53 +0800

    Update p035_BPNetworkForIRIS.py

--- learn_tf/p035_BPNetworkForIRIS.py
+++ learn_tf/p035_BPNetworkForIRIS.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+from sklearn.model_selection import train_test_split
 
 GPU_CONFIG = tf.ConfigProto()
 GPU_CONFIG.gpu_options.allow_growth=True
@@ -14,8 +15,8 @@ GPU_CONFIG.gpu_options.allow_growth=True
 # containing 3 classes of flowers "Iris setosa" "Variegated Iris" "Virginia iris"
 # with feature about "sepal length" "sepal width" "petal length" "petal width"
 
-def run(h_size, stddev, sgd_setp):
-    pass
+RANDOMSEED = 40
+tf.set_random_seed(RANDOMSEED)
 
 def load_iris_date():
     data = np.genfromtxt('IRIS_data\iris.csv', delimiter=',')
@@ -25,11 +26,30 @@ def load_iris_date():
     all_X = np.ones((L,W+1))
     all_X[:,1:] = data
     num_labels = len(np.unique(target)) # np.unique(): remove repetitive elements and sort
-    all_Y = np.eye(num_labels)[target]
-    
+    all_y = np.eye(num_labels)[target]
+    return train_test_split(all_X,all_y,test_size=0.33,random_state=RANDOMSEED)
     
 
+# make network 
+def initialize_weights(shape, stddev):
+    weights = tf.random_normal(shape, stddev=stddev)
+    return tf.Variable(weights)
+
+def forward_propagation(X, weights_1, weights_2):
+    sigmoid = tf.nn.sigmoid(tf.matmul(X,weights_1))
+    y = tf.matmul(sigmoid, weights_2)
+    return y
+
+
 def run(h_size, stddev, sgd_steps):
+    train_x,test_x,train_y,test_y = load_iris_date()
+
+    # size of layers
+    x_size = train_x.shape[1] # 4 features and 1 bias
+    h_size = 256
+    y_size = train_y.shape[1] # 3 classes
+
+    #cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_size,log))
     test_accs = []
     train_accs = []
     time_taken_summary = []

26a539958eb9673d666d013e3e19007053983972
lee-pc\admin <584897688@qq.com>
2019-10-28 18:50:24 +0800

    Update p076_conv2d.py

--- learn_module/learn_tf/p076_conv2d.py
+++ learn_module/learn_tf/p076_conv2d.py
@@ -3,7 +3,11 @@
 # authors: Manpreet Singh Ghotra, Rajdeep Dua
 
 import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
+
+
+
 
 GPU_CONFIG = tf.ConfigProto()
 GPU_CONFIG.gpu_options.allow_growth=True
@@ -84,40 +88,64 @@ def test_strides_and_padding():
         print('    2. find the value according to the strides')
   
 
-def train_mnist():
+def test_train_mnist():
     batch_size = 50
     learning_rate = 0.01
-    input = tf.placeholder(tf.float32,[batch_size,28,28,1])
+    image = tf.placeholder(tf.float32,[batch_size,28,28,1])
     label = tf.placeholder(tf.float32,[batch_size,10])
-    ker1 = tf.random_uniform([1, 5, 5, 12],tf.float32)
-    bias1 = tf.zeros((12,1,1,1),tf.float32)
-    conv1 = tf.nn.conv2d(input, ker1)
+    ker1 = tf.random_uniform([5, 5, 1, 12], minval=0.1, maxval=0.5, dtype=tf.float32)
+    ker1 = tf.Variable(ker1)
+    bias1 = tf.Variable(tf.zeros((1,1,1,12),tf.float32))
+    conv1 = tf.nn.conv2d(image, ker1, strides=[1,1,1,1], padding='SAME')
     relu1 = tf.nn.relu(conv1+bias1)
-    pool1 = tf.nn.max_pool(relu1,(1,2,2,1),strides=(1,2,2,1))
-    ker2 = tf.random_uniform([12, 5, 5, 20],tf.float32)
-    bias2 = tf.zeros((20,1,1,1),tf.float32)
-    conv2 = tf.nn.conv2d(pool1, ker2)
+    pool1 = tf.nn.max_pool(relu1,(1,2,2,1),strides=(1,2,2,1),padding='SAME')
+    ker2 = tf.random_uniform([5, 5, 12, 20], minval=0.1, maxval=0.5, dtype=tf.float32)
+    ker2 = tf.Variable(tf.Variable(ker2))
+    bias2 = tf.zeros((1,1,1,20),tf.float32)
+    conv2 = tf.nn.conv2d(pool1, ker2, strides=[1,1,1,1], padding='SAME')
     relu2 = tf.nn.relu(conv2+bias2)
-    pool2 = tf.nn.max_pool(relu2,(1,2,2,1),strides=(1,2,2,1)).reshape(batch_size,-1)
-    wfc1 = tf.random_uniform([pool2.shape[0],pool2.shape[1],100],tf.float32)
-    bfc1 = tf.zeros(wfc1.shape,tf.float32)
-    fc1 = pool2*wfc1+bfc1
+    pool2 = tf.nn.max_pool(relu2,(1,2,2,1),strides=(1,2,2,1),padding='SAME')
+    pool2 = tf.reshape(pool2,(batch_size,-1))
+    wfc1 = tf.random_uniform((980,100), minval=0.1, maxval=0.5, dtype=tf.float32)
+    wfc1 = tf.Variable(wfc1)
+    bfc1 = tf.Variable(tf.zeros((batch_size,100),tf.float32))
+    fc1 = tf.matmul(pool2,wfc1)+bfc1
     relu3 = tf.nn.relu(fc1)
-    wfc2 = tf.random_uniform([relu3.shape[0],relu3.shape[1],10],tf.float32)
-    bfc2 = tf.zeros(wfc2.shape,tf.float32)
-    fc2 = relu3*wfc2+bfc2
+    wfc2 = tf.random_uniform((100,10), minval=0.1, maxval=0.5, dtype=tf.float32)
+    wfc2 = tf.Variable(wfc2)
+    bfc2 = tf.Variable(tf.zeros((batch_size,10),tf.float32))
+    fc2 = tf.matmul(relu3,wfc2)+bfc2
+
     softmaxloss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=label,logits=fc2)
     step = tf.train.GradientDescentOptimizer(learning_rate).minimize(softmaxloss)
+
+    # sess = tf.InteractiveSession()
+    acct_mat = tf.equal(tf.arg_max(softmaxloss,1),tf.arg_max(label,1))
+    acct_ret = tf.reduce_sum(tf.cast(acct_mat,tf.float32))
+    sess.run(tf.global_variables_initializer())
+
+    data = input_data.read_data_sets('MNIST_data/',one_hot=True)
+
     for i in range(1000):
-        
+        batch_xs, batch_ys = data.train.next_batch(batch_size)
+        batch_xs = batch_xs.reshape((batch_size,28,28,1))
+        sess.run(step, feed_dict={image:batch_xs,label:batch_ys})
+        if i%10000==0:
+            res = sess.run(acct_ret,feed_dict={
+                  image:data.test.images.reshape((-1,28,28,1))[:50],\
+                  label:data.test.labels[:50]})
+            print(res)
     
 
 
 
 
-test_whether_rotate_kernel()
+#test_whether_rotate_kernel()
+#print('')
+#test_strides_and_padding()
+#print('')
+test_train_mnist()
 print('')
-test_strides_and_padding()
-print('')
+
 sess.close()     
 
